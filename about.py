b4c61b6e4cf466d5278c841f12c1ce57a3787010
daniel <dr.vasilevskij@gmail.com>
2020-11-28 18:31:26 +0300

    добавил дз

---
+++ about.py
@@ -0,0 +1,18 @@
+
+myGreeting = "Хеллоу,"
+myName = "Василевский,"
+mySurname = "Даниил,"
+myAge = 17
+myBorn = "Великий Новгород,"
+myStudy = "Строительном колледже."
+myLive = "Великий Новгород."
+myHobby = "программированием."
+myRareHobby = 1.5
+myAccess = "Постоянно"
+myActivity = "рыбалку,"
+difСity = "Боровичах, Питере."
+myFavorite = "Ильмень."
+print(myGreeting, "меня зовут", myName, mySurname, "мне", myAge, "лет.", "Я родился в городе",  myBorn, "учусь в",
+      myStudy, "В данный момент живу в городе", myLive, "В свободное время занимаюсь", myHobby,
+      "Я ", myAccess, "нахожу время для своего хобби,",
+       "Ещё я люблю", myActivity, "особенно на озере", myFavorite  "Кроме Новгорода, я был ещё в", difСity)
